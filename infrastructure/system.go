31c2d1ea640b941dbc571ca1835d70cd34439c65
Tom Knight <tomknight.dev@gmail.com>
2021-12-29 23:48:34 +0000

    Added cpu models

--- infrastructure/system.go
+++ infrastructure/system.go
@@ -1,5 +1,7 @@
 package infrastructure
 
+import "fmt"
+
 type System struct {
 	SystemId    int
 	SystemName  string
@@ -16,7 +18,15 @@ func NewSystem(systemId int, systemName string, zoneAddress ZoneAddress) *System
 	sys.SystemName = systemName
 	sys.ZoneAddress = zoneAddress
 
+	// Defaults
+	sys.AddMemory(Ram{RamId: 0, RamSize: 1024, RamSpeed: 1333})
+	sys.AddCpu(Cpu{CpuId: 0, CpuModel: CpuModels["12600K"]})
+
 	return sys
+}
+
+func (s *System) AddCpu(cpu Cpu) {
+	s.Processor = append(s.Processor, cpu)
 }
 
 func (s *System) AddMemory(mem Ram) {
@@ -30,4 +40,18 @@ func (s *System) GetTotalMemory() int {
 	}
 
 	return total
+}
+
+func (s *System) GetCpuInfo() string {
+	info := fmt.Sprintf("- Cpu: %s", s.Processor[0].CpuModel.CpuModelName)
+	info += fmt.Sprintf("\n- Manufacturer: %s", s.Processor[0].CpuModel.CpuModelManufacturer.String())
+	info += fmt.Sprintf("\n- Clock: %f", s.Processor[0].CpuModel.CpuModelClock)
+	return info
+}
+
+func (s *System) GetSystemInfo() string {
+	info := s.GetCpuInfo()
+	info += "\n" + fmt.Sprintf("- Ram: %d", s.GetTotalMemory())
+
+	return info
 }
